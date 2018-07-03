2a6a29f627ced33552cd90ab73cc91a56538f00f
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-03 20:04:00 +0300

    Created static class HTTPRequests

--- ConsoleAppRequestsLinq/ConsoleAppRequestsLinq/Program.cs
+++ ConsoleAppRequestsLinq/ConsoleAppRequestsLinq/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ConsoleAppRequestsLinq
 {
     class Program
-    {
+    {       
+
         static void Main(string[] args)
         {
+            Task<List<User>> t1 = HTTPRequests.GetUsers();
+            t1.Wait();
+
+            Task<List<Post>> t2 = HTTPRequests.GetPosts();
+            t2.Wait();
+
+            Task<List<Comment>> t3 = HTTPRequests.GetComments();
+            t3.Wait();
+
+            Task<List<Todo>> t4 = HTTPRequests.GetTodos();
+            t4.Wait();
+
         }
     }
 }
