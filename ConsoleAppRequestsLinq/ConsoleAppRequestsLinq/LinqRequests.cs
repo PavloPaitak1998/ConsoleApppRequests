c3f715cadd0803a5d87f129ecaa467d12e3e5b6e
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-03 20:24:16 +0300

    Created static class LinqRequests with 1 request

---
+++ ConsoleAppRequestsLinq/ConsoleAppRequestsLinq/LinqRequests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRequestsLinq
+{
+    static class LinqRequests
+    {
+        //1
+        static IEnumerable<(Post post, int count)> CommentsCount(int id, IEnumerable<Post> _postsEntity)
+        {
+            return _postsEntity.Where(p => p.UserId == id).Select(p => (Post: p, Count: p.Comments.Count()));
+        }
+
+    }
+}
