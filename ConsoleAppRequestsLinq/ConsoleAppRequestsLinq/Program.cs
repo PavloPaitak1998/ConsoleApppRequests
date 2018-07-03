002cdc64a2e801022920a919c7277a7df7371fa9
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-03 20:17:58 +0300

    Created static methods creating posts and users entities

--- ConsoleAppRequestsLinq/ConsoleAppRequestsLinq/Program.cs
+++ ConsoleAppRequestsLinq/ConsoleAppRequestsLinq/Program.cs
@@ -25,6 +25,52 @@ namespace ConsoleAppRequestsLinq
             Task<List<Todo>> t4 = HTTPRequests.GetTodos();
             t4.Wait();
 
+            var users = t1.Result;
+            var posts = t2.Result;
+            var comments = t3.Result;
+            var todos = t4.Result;
+
+            List<Post> postsEntity = GetPostsEntity(posts, comments);
+            List<User> usersEntity = GetUsersEntity(users, postsEntity, todos);
+
         }
+
+        static List<Post> GetPostsEntity(List<Post> _posts, List<Comment> _comments)
+        {
+            var postsEntity = (from p in _posts
+                               join c in _comments on p.Id equals c.PostId into postComments
+                               select new Post
+                               {
+                                   Id = p.Id,
+                                   Body = p.Body,
+                                   Title = p.Title,
+                                   CreatedAt = p.CreatedAt,
+                                   Likes = p.Likes,
+                                   UserId = p.UserId,
+                                   Comments = postComments.ToList()
+                               }).ToList();
+
+            return postsEntity;
+        }
+
+        static List<User> GetUsersEntity(List<User> _users, List<Post> _postsEntity, List<Todo> _todos)
+        {
+            var usersEntity = (from u in _users
+                               join p in _postsEntity on u.Id equals p.UserId into userPosts
+                               join t in _todos on u.Id equals t.UserId into userTodos
+                               select new User
+                               {
+                                   Id = u.Id,
+                                   CreatedAt = u.CreatedAt,
+                                   Avatar = u.Avatar,
+                                   Email = u.Email,
+                                   Name = u.Name,
+                                   Posts = userPosts.ToList(),
+                                   Todos = userTodos.ToList()
+                               }).ToList();
+
+            return usersEntity;
+        }
+
     }
 }
