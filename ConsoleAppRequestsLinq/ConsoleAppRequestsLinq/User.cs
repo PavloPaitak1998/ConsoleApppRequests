7b8505fc7f486a0bc28e599deee867a3b155f2e7
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-03 19:42:29 +0300

    Created class User

---
+++ ConsoleAppRequestsLinq/ConsoleAppRequestsLinq/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRequestsLinq
+{
+    sealed class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public string CreatedAt { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}; Name:{Name}; Avatar:{Avatar}; Email:{Email}; CreatedAt:{CreatedAt}";
+        }
+    }
+}
